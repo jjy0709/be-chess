d615fcd8eec0d4af6391c6ea2a6f8dfdd90466b8
jjy0709 <jjy0709@kaist.ac.kr>
2023-07-10 17:52:56 +0900

    King 이동 구현 (리팩토링 전)

--- src/main/java/chess/Board.java
+++ src/main/java/chess/Board.java
@@ -6,6 +6,7 @@ import java.util.*;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
+import static chess.Position.distance;
 import static utils.StringUtils.appendNewLine;
 
 public class Board {
@@ -46,6 +47,22 @@ public class Board {
     public void move(String src, String des) {
         move(des, getPieceAt(src));
         move(src, Piece.createBlank());
+    }
+
+    public void moveKing(String src, String des) throws Exception {
+        Position pSrc = new Position(src);
+        Position pDes = new Position(des);
+        if(!pDes.inBoard()) throw new Exception("Destination out of the board!");
+        if(distance(pSrc,pDes) != 1) throw new Exception("You cannot move king over one block!");
+        if(getPieceAt(src).getColor() == getPieceAt(des).getColor()) throw new Exception("You cannot move your piece to already existing location");
+        move(src, des);
+    }
+
+
+    private boolean checkMovePossible(String loc, Piece piece) {
+        Piece piece1 = getPieceAt(loc);
+        if(piece1.getColor() == piece.getColor()) return false;
+
     }
 
 }
--- src/main/java/chess/Position.java
+++ src/main/java/chess/Position.java
@@ -9,4 +9,13 @@ public class Position {
         this.rank = 8 - tmp;
         this.colum = loc.charAt(0) - 'a';
     }
+
+    public static int distance(Position src, Position des) {
+        return Math.max(Math.abs(src.rank - des.rank), Math.abs(src.colum - des.colum));
+    }
+
+    public boolean inBoard() {
+        if(this.rank > 8 || this.rank < 0 || this.colum > 8 || this.colum < 0) return false;
+        return true;
+    }
 }
