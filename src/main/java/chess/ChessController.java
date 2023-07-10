a49b5f723a82b4a2dcb75e5e95426fc9a4e536bf
jjy0709 <jjy0709@kaist.ac.kr>
2023-07-10 10:47:41 +0900

    2. Add move to main method in controller

--- src/main/java/chess/ChessController.java
+++ src/main/java/chess/ChessController.java
@@ -4,18 +4,22 @@ import java.util.Scanner;
 
 public class ChessController {
     public static void main(String[] args) {
-        Board board;
+        Board board = new Board();
         Scanner sc = new Scanner(System.in);
-        String input = sc.next();
+        String input = sc.nextLine();
 
         while(true){
             if(input.equals("start")){
-                board = new Board();
                 board.initialize();
                 System.out.println(board.print());
             }
+            if(input.startsWith("move")){
+                String[] ipts = input.split(" ");
+                board.move(ipts[1], ipts[2]);
+                System.out.println(board.print());
+            }
             if(input.equals("end")) return;
-            input = sc.next();
+            input = sc.nextLine();
         }
 
     }
